ceea1a30dc98df77e30beb6f27fc8f6bbbc4494e
michaelroesinger <mroesinger@gmail.com>
2023-04-04 16:33:19 -0700

    changed copy

--- src/components/About.js
+++ src/components/About.js
@@ -8,12 +8,15 @@ export default function About() {
           src="https://avatars.githubusercontent.com/u/106269063?v=4"
         />
         <div id="about-info">
-          Boom-da-da-mmm-dum-na-ee-ma Da-boom-da-da-mmm-dum-na-ee-ma
-          Da-boom-da-da-mmm-dum-na-ee-ma Da-boom-da-da-mmm-dum-na-ee-ma
-          Da-boom-da-da-mmm-dum-na-ee-ma Da-boom-da-da-mmm-dum-na-ee-ma
-          Da-boom-da-da-mmm-dum-na-ee-ma Da-boom-da-da-mmm-dum-na-ee-ma
-          Da-boom-da-da-mmm-dum-na-ee-ma Da-boom-da-da-mmm-dum-na-ee-ma
-          Da-boom-da-da-mmm-dum-na-ee-ma Da-boom-da-da-mmm-dum-na-ey Go! -KORN
+          Full stack web developer and life long problem solver. Emerging from a
+          career in German automobile restoration, I've shifted gears into the
+          world of Software Engineering. I was introduced to coding while
+          visiting my best friend from music school in Berlin, Germany. Having
+          honed my engineering skills in the automotive industry, my transition
+          to the tech world has been seamless. Everything I loved about,
+          diagnosing fuel injection systems, tracing circuits, and repairing
+          faulty electronics is all neatly encompassed in the process of
+          debugging. Here's to trading dirty hands for clean code! 🍻
         </div>
       </div>
     </div>
