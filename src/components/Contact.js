c47b203ec088020ccf5402b76a35e0cecdd790a6
michaelroesinger <mroesinger@gmail.com>
2023-04-11 18:49:27 -0700

    contact update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,8 +2,9 @@ export default function Contact() {
   return (
     <div id="contact">
       <div id="contact-container">
-        mroesinger@gmail.com linkedin.com/michael-roesinger
-        github.com/mroesinger
+        <button onclick="window.location.href='https://www.linkedin.com/'">
+          {/* <img src="https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/github-512.png" alt="Visit my LinkedIn profile"> */}
+        </button>
       </div>
     </div>
   );
