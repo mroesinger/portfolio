6b4ba765b399c791f17e374c232dbc39e71ed243
michaelroesinger <mroesinger@gmail.com>
2023-04-12 17:05:26 -0700

    linkedin update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,20 @@ export default function Contact() {
   return (
     <div id="contact">
       <div id="contact-container">
-        <button onclick="window.location.href='https://www.linkedin.com/'">
-          {/* <img src="https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/github-512.png" alt="Visit my LinkedIn profile"> */}
-        </button>
+        <a href="https://www.github.com/">
+          <img
+            id="github-icon"
+            alt="github cat"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4H3kSxWJ6ATMIjA-l52rsGhHX_op9lusqeetVIaWFxw&s"
+          ></img>
+        </a>
+        {/* <a href="https://www.linkedin.com/">
+          <img
+            id="linkedin-icon"
+            alt="linkedin icon"
+            src="https://cdn-icons-png.flaticon.com/512/1384/1384014.png"
+          ></img>
+        </a> */}
       </div>
     </div>
   );
