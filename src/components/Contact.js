123e6399b95cb993ff739e67ee895bfc4066946d
michaelroesinger <mroesinger@gmail.com>
2023-04-18 14:35:19 -0700

    updated contacts

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,50 +1,3 @@
-// import React, { useState } from "react";
-
-// export default function Contact() {
-//   function copyToClipboard(text) {
-//     navigator.clipboard.writeText(text);
-//   }
-
-//   function EmailIcon() {
-//     const handleClick = () => {
-//       copyToClipboard("weee");
-//     };
-//   }
-//   return (
-//     <div id="contact">
-//       <div id="contact-container">
-//         <div id="github-container">
-//           <a href="https://www.github.com/">
-//             <img
-//               id="github-icon"
-//               alt="github cat"
-//               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4H3kSxWJ6ATMIjA-l52rsGhHX_op9lusqeetVIaWFxw&s"
-//             ></img>
-//           </a>
-//         </div>
-//         <div id="linkedin-container">
-//           <a href="https://www.linkedin.com/">
-//             <img
-//               id="linkedin-icon"
-//               alt="linkedin icon"
-//               src="https://cdn3.iconfinder.com/data/icons/sociocons/256/linkedin-sociocon.png"
-//             ></img>
-//           </a>
-//         </div>
-//         <div id="email-container">
-//           <a href="#" onClick={handleClick}>
-//             <img
-//               id="email-icon"
-//               alt="email icon"
-//               src="https://downloadr2.apkmirror.com/wp-content/uploads/2020/10/Gmail_round.png"
-//             ></img>
-//           </a>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
 import React from "react";
 
 export default function Contact() {
@@ -54,14 +7,13 @@ export default function Contact() {
 
   function handleEmailClick() {
     copyToClipboard("mroesinger@gmail.com");
-    window.alert("mroesinger@gmail.com copied to clipboard");
   }
 
   return (
     <div id="contact">
       <div id="contact-container">
         <div id="github-container">
-          <a href="https://www.github.com/">
+          <a href="https://www.github.com/mroesinger">
             <img
               id="github-icon"
               alt="github cat"
@@ -70,7 +22,7 @@ export default function Contact() {
           </a>
         </div>
         <div id="linkedin-container">
-          <a href="https://www.linkedin.com/">
+          <a href="https://www.linkedin.com/in/michael-roesinger">
             <img
               id="linkedin-icon"
               alt="linkedin icon"
