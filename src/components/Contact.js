b3db5375353c2db8c6cbeac957803e215ddd9d46
michaelroesinger <mroesinger@gmail.com>
2023-04-19 15:06:19 -0700

    added copy to clipboard text for email

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,61 @@
-import React from "react";
+// import React from "react";
+
+// export default function Contact() {
+//   function copyToClipboard(text) {
+//     navigator.clipboard.writeText(text);
+//   }
+
+//   function handleEmailClick() {
+//     copyToClipboard("mroesinger@gmail.com");
+//   }
+
+//   return (
+//     <div id="contact">
+//       <div id="contact-container">
+//         <div id="github-container">
+//           <a href="https://www.github.com/mroesinger">
+//             <img
+//               id="github-icon"
+//               alt="github cat"
+//               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4H3kSxWJ6ATMIjA-l52rsGhHX_op9lusqeetVIaWFxw&s"
+//             />
+//           </a>
+//         </div>
+//         <div id="linkedin-container">
+//           <a href="https://www.linkedin.com/in/michael-roesinger">
+//             <img
+//               id="linkedin-icon"
+//               alt="linkedin icon"
+//               src="https://cdn3.iconfinder.com/data/icons/sociocons/256/linkedin-sociocon.png"
+//             />
+//           </a>
+//         </div>
+//         <div id="email-container">
+//           <a href="#" onClick={handleEmailClick}>
+//             <img
+//               id="email-icon"
+//               alt="email icon"
+//               src="https://downloadr2.apkmirror.com/wp-content/uploads/2020/10/Gmail_round.png"
+//             />
+//           </a>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+import React, { useState } from "react";
 
 export default function Contact() {
+  const [isEmailCopied, setIsEmailCopied] = useState(false);
+
   function copyToClipboard(text) {
     navigator.clipboard.writeText(text);
+    setIsEmailCopied(true);
   }
 
-  function handleEmailClick() {
+  function handleEmailClick(event) {
+    event.preventDefault();
     copyToClipboard("mroesinger@gmail.com");
   }
 
@@ -39,6 +89,7 @@ export default function Contact() {
             />
           </a>
         </div>
+        {isEmailCopied && <p>mroesinger@gmail.com copied to clipboard!</p>}
       </div>
     </div>
   );
