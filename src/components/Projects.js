1ff57f6d8a773b43c50e597c4e858500b938ad8c
michaelroesinger <mroesinger@gmail.com>
2023-04-19 11:52:35 -0700

    added synth swap

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,7 +9,7 @@ export default function Projects() {
         />
       </div> */}
 
-      <div id="westy-rentals-vid">
+      <div id="westy-rentals-container">
         <a href="https://www.loom.com/share/7f5f2c913b854e7d84dc7d44a838436d">
           <img
             id="westy-rentals-gif"
@@ -22,6 +22,20 @@ export default function Projects() {
           website where my friends in the van community could rent their
           westfalias to one another. The app allows users to interact with each
           other and rent vans from van owners for their own camping adventures.
+        </div>
+      </div>
+      <div id="synth-swap-container">
+        <img
+          id="synth-swap-image"
+          src="synthswap.png"
+          alt="synth swap screenshot"
+        />
+        <div>
+          Playing, fixing, buying, selling and trading vintage analog
+          synthesizers has been a passion of mine for all of my adult life. For
+          my very first JavaScript project, I made a simple p2p app, where San
+          Francisco bay area users could host their synths for free as an
+          alternative to craigslist
         </div>
       </div>
     </div>
