9143aa35cff70f1ad9fc4b7ae404c3ea24136fa7
michaelroesinger <mroesinger@gmail.com>
2023-05-11 19:25:24 -0700

    nav opacity

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div id="home">
       <div id="home-image-container">
-        <img id="home-image" alt="3.2 engine" src="flatsixpatent.jpg" />
+        <img id="home-image" alt="911 engine" src="flatsixpatent.jpg" />
       </div>
     </div>
   );
