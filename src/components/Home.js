d1e6611bfb52a52d69a34f247396a93e32bdf49b
michaelroesinger <mroesinger@gmail.com>
2023-06-08 16:26:01 -0700

    title change

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,10 @@
 export default function Home() {
   return (
     <div id="home">
+      <div id="intro-container">
+        <div id="intro-name">Michael Roesinger</div>
+        <div id="intro-text">Programmer | Mechanic | Camper | Musician </div>
+      </div>
       <div id="home-image-container">
         <img id="home-image" alt="911 engine" src="flatsixpatent.jpg" />
       </div>
