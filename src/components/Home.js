2278417ba98b55844e5e65867004e79359fe653c
michaelroesinger <mroesinger@gmail.com>
2024-03-27 14:31:50 -0700

    testing3

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,12 +6,10 @@ export default function Home() {
         <div id="intro-underline">
           __________________________________________________________________________
         </div>
-        <div id="intro-text">
-          Programmer | Mechanic | Musician | Camper | Corgi-Fan
-        </div>
+        <div id="intro-text">Programmer | Mechanic | Camper | Musician</div>
       </div>
       <div id="home-image-container">
-        <img id="home-image" alt="911 engine" src="blueprint.jpeg" />
+        <img id="home-image" alt="911 engine" src="flatsixpatent.jpg" />
       </div>
     </div>
   );
